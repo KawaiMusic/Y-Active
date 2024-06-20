fbea2a609b5f8bafdd61953095a0842f0f9462d9
RAMON KAWAI <RAMONKAWAY74@GMAIL.COM>
2024-06-20 12:59:51 -0300

    att

--- src/Pages/Splashart/index.js
+++ src/Pages/Splashart/index.js
@@ -1,10 +1,11 @@
-import Logo from './Logo.png'
+import Logo from './image.png'
+import style from "./style.css"
 
 function Splashart (){
   return(
     <div className="divSplash">
 
-      <img src= {Logo} alt='Logo' className='logo' />
+      <img src= {Logo} alt='Logo' className ='logo' />
     </div>
 
   )
